a4f5b1cc3a402f54ab89134cfce4ab7605b4f135
Craig <craigk00@hotmail.com>
2025-03-23 17:37:09 -0400

    background gifs work!

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
+"use client";
 import ProjectCard from "@/components/ProjectCard";
 import FooterLink from "@/components/FooterLink";
-import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+    const [bgClassName, SetBg] = useState("");
+
     return (
-        <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            {/* grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] */}
-            <div className="">
+        <div className={`min-h-screen p-8 pb-20 gap-16 sm:p-20 ${bgClassName}`}>
+            <div className="opacityOverride">
                 <ProjectCard
+                    setBg={SetBg}
                     projTitle="CodingCards"
                     projDesc="Coding cards is a roguelike deckbuilder where the cards have lines of code. It's currently my main project!"
                     buttonRef="/projects/coding_cards"
-                    buttonText="Visit project page"
+                    bgClassName="bgA"
                 />
                 <ProjectCard
+                    setBg={SetBg}
                     projTitle="Minesweeper"
                     projDesc="I recreated the classic game Minesweeper! I did just about everything in a single weekend, with some quality of life/minor bug fixes later. You can learn all about how I did this here!"
                     buttonRef="/projects/minesweeper"
-                    buttonText="Visit project page"
+                    bgClassName="bgB"
                 />
             </div>
             <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,22 +1,30 @@
+"use client";
 import Button from "./ui/Button";
 
 interface ProjectCardProps {
+    setBg: (className: string) => void;
     projTitle: string;
     projDesc: string;
     buttonRef: string;
-    buttonText?: string;
+    bgClassName?: string;
 }
 
 // TODO:: make an on-hover effect that changes the background of the entire screen play a GIF of the project
 
 export default function ProjectCard({
+    setBg,
     projTitle,
     projDesc,
     buttonRef,
-    buttonText = "",
+    bgClassName = "bgDefault",
 }: ProjectCardProps) {
     return (
-        <a className="" href={buttonRef}>
+        <a
+            className=""
+            href={buttonRef}
+            onMouseEnter={() => setBg(bgClassName)}
+            onMouseLeave={() => setBg("bgDefault")}
+        >
             <div className="transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] dark:hover:text-gray-900 mb-16">
                 {/* <Image></Image> */}
                 <h2>{projTitle}</h2>
