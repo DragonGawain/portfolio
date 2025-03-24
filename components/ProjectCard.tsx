f4f13ce6eca0ebe73d2d6911a4577c7075b77224
Craig <craigk00@hotmail.com>
2025-03-24 14:32:18 -0400

    button variants

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -29,51 +29,27 @@ export default function ProjectCard({
         >
             <div className="flex mb-16">
                 <table>
-                    <tr>
-                        <td className="w-48 h-48">
-                            <Image
-                                // className="center"
-                                aria-hidden
-                                src={imgSrc}
-                                alt={imgAltText}
-                                width={192}
-                                height={192}
-                            />
-                        </td>
-                        {/* This table column serves as a spacer between the image and the text. */}
-                        <td className="w-5"></td>
-                        <td className="align-top">
-                            {/* <div className=""> */}
-                            <h2>{projTitle}</h2>
-                            <p>{projDesc}</p>
-                        </td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td className="w-48 h-48">
+                                <Image
+                                    aria-hidden
+                                    src={imgSrc}
+                                    alt={imgAltText}
+                                    width={192}
+                                    height={192}
+                                />
+                            </td>
+                            {/* This table column serves as a spacer between the image and the text. */}
+                            <td className="w-5"></td>
+                            <td className="align-top">
+                                <h2>{projTitle}</h2>
+                                <p>{projDesc}</p>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </a>
     );
 }
-
-// <div className="mr-4 w-72 h-72 overflow-x-hidden">
-//                     {/* <Image
-//                         // className="center"
-//                         aria-hidden
-//                         src={imgSrc}
-//                         alt={imgAltText}
-//                         width={320}
-//                         height={320}
-//                     /> */}
-//                 </div>
-//                 {/* <div
-//                     className={`w-72 h-72 mr-4 background-size:cover bg-[url(@/public${imgSrc})]`}
-//                 /> */}
-
-//                 {/* </div> */}
-//                 {/*transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] dark:hover:text-gray-900 */}
-//                 <div className="block">
-//                     {/* <Image></Image> */}
-//                     <h2>{projTitle}</h2>
-//                     {/* <div> */}
-//                     <p>{projDesc}</p>
-//                     {/* </div> */}
-//                 </div>
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,14 +1,33 @@
-interface ButtonProps {
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+    "rounded-full border border-solid border-transparent transition-colors flex items-center justify-center gap-2 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto mb-8 max-w-56",
+    {
+        variants: {
+            variant: {
+                default:
+                    "bg-[#0a0a0a] dark:bg-[#ededed] hover:bg-[#383838] dark:hover:bg-[#cccccc] text-[#fefefe] dark:text-[#171717] hover:text-[#fefefe] dark:hover:text-[#171717]",
+                git: "bg-[#0a0a0a] dark:bg-[#ededed] hover:bg-[#383838] dark:hover:bg-[#cccccc] text-[#fefefe] dark:text-[#171717] hover:text-[#fefefe] dark:hover:text-[#171717]",
+                itch: "bg-[#ff7878] dark:bg-[#7f1a1a] hover:bg-[#ff8e8e] dark:hover:bg-[#5c0f0f] text-[#171717] dark:text-[#fefefe] hover:text-[#171717] dark:hover:text-[#fefefe]",
+            },
+        },
+    }
+);
+
+export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+        VariantProps<typeof buttonVariants> {
     href: string;
     text: string;
 }
 
-export default function Button({ href, text = "" }: ButtonProps) {
+export default function Button({
+    variant = "default",
+    href,
+    text = "",
+}: ButtonProps) {
     return (
-        <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto mb-8 max-w-56"
-            href={href}
-        >
+        <a className={buttonVariants({ variant })} href={href}>
             {text}
         </a>
     );
